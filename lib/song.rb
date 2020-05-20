e6b1fac3432aecb59fcafdff1a072eb367064270
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-20 20:00:36 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+  attr_accessor :name, :artist
+@@all = []
+
+def self.all
+  @@all
+end
+
+def save
+  self.class.all << self
+end
+
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(file)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
+    song
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
+
+end
